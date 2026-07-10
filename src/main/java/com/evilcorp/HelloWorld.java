9b2910c66ef8e805e3ea736b59d5c1d50e408595
Fentomi <105577819+Fentomi@users.noreply.github.com>
2026-07-10 21:44:05 +0300

    initial commit + kata (hello world)

---
+++ src/main/java/com/evilcorp/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.evilcorp;
+
+public class HelloWorld {
+  // Write a public static function `greet` that returns "hello world!"
+  public static String greet() {
+    return "hello world!";
+  }
+}
